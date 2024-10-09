81cdb4ee3fe8c73d50de818f0c6bbee944906f1e
De1niz <erdogde4957_aehit@students.vizja.pl>
2024-10-10 01:09:24 +0200

    Updated

--- C++Workspace/main.cpp
+++ C++Workspace/main.cpp
@@ -6,9 +6,20 @@
 //
 
 #include <iostream>
-
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+ 
+void checktriangle(int a, int b, int c) {
+    if (a+b>c && a+c>b && b+c>a){
+    printf("that lines can form triangle \n");
+}
+else {
+    printf("that lines can't form triangle \n");
+}
+}
+ 
+int main()
+{
+  checktriangle(10, 10, 10);
+  checktriangle(30, 10, 10);
+  checktriangle(50, 10, 10);
     return 0;
 }
